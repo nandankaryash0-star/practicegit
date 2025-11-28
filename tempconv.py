22aae02e3902c6cf15650406ed2f0e4dabc941bd
nandankaryash0-star <nandankaryash0@gmail.com>
2025-11-28 06:03:36 +0530

    Temperature Converter

---
+++ tempconv.py
@@ -0,0 +1,15 @@
+# Temperature converter 
+
+unit = input("Enter the unit of Temperature Celcius or Fahrenheit (C or F): ")
+temp = float(input("Enter the temperature:  "))
+
+if unit == "C":
+    temp = round((9* temp)/5 + 32,1)
+    print(f"The temperature in Fahrenheit is {temp}")
+
+elif unit == "F":
+    temp = round((temp - 32)*5/9,1)
+    print(f"The temperature in Celcius is {temp}")
+
+else:
+    print(f"The given unit {unit} is invalid")
